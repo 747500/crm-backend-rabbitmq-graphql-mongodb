9b6b67554d52d42027cf41b4eefcfcb8c77eedf0
Serge R <747500@gmail.com>
2021-03-19 08:19:15 +0300

    initial services

---
+++ services/index.mjs
@@ -0,0 +1,46 @@
+
+
+import amqp from './amqp/index.mjs'
+
+const Services = [
+	amqp,
+	telegram,
+]
+
+function run (service) {
+
+	console.log(`sss init "${service.name}"`)
+
+	return service.init.bind(this)().then(
+		result => {
+			console.log('\tok')
+			service.endpoint = result
+			return result
+		}
+	)
+}
+
+function init () {
+	const s = {}
+
+	return Services.reduce(
+		(ok, service) => ok.then(
+			() => run.bind(s)(service).then(() => {
+				return s[service.name] = service.endpoint
+			})
+		),
+		Promise.resolve()
+	)
+	.then(() => {
+		console.log('* Services started:', Object.keys(s))
+		return s;
+	})
+}
+
+const ok = init() // promise-singleton
+
+export default {
+	Run () {
+		return ok
+	}
+}
